a15567963bd5401cea652098561e44dcd2ba7a8e
ebrahim.j <ebrahim.janoowalla@andela.com>
2017-08-03 11:40:08 +0300

    add the missing file field

--- opengazettes/opengazettes/items.py
+++ opengazettes/opengazettes/items.py
@@ -10,11 +10,12 @@ import scrapy
 
 class OpengazettesItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+
     gazette_link = scrapy.Field()
     publication_date = scrapy.Field()
     gazette_number = scrapy.Field()
     file_urls = scrapy.Field()
+    files = scrapy.Field()
     filename = scrapy.Field()
     gazette_title = scrapy.Field()
     gazette_day = scrapy.Field()
